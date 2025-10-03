eda374d1d161f4a3d105072dc3fd6907dc2d0364
Amin Smith <aminsmith128@gmail.com>
2025-10-03 13:01:27 -0400

    raycast change
    didnt upload last commit

--- raycast.js
+++ raycast.js
@@ -1,8 +1,14 @@
-function castRay(x, y, dir){// returns distance
+class Raycast {
+    static getDistance(ping, ping2){
 
-    let cos = Math.cos(dir)
-    let sin = Math.sin(dir)
-    let iterations = 10
-    
+        // this is not a true distance but rather an approximation
 
+        // ping is a ping from Echolocator.pings
+
+        if(ping[2] == 0){// angle 0 special case
+            return Math.abs(ping2[1]-ping[1])
+        }
+
+        return Math.abs((ping2[0] - ping[0]) - (ping2[1] - ping[1])/Math.atan(ping[2]))
+    }
 }
